9ee36f1d502f3a369c1c49df69c83f35e62ee39b
Liviadbelao <125027552+Liviadbelao@users.noreply.github.com>
2023-05-15 15:17:25 -0300

    fix adicionar 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,40 +3,43 @@ var setB = []
 var ganhosB = 0
 
 function adicionarB() {
-    if (contB != 25) {
+    if (contB != ganhar) {
         contB++
         document.getElementById("valorB").innerHTML = contB
     }
-    if (contB == 25 && sets < 5) {
-        setA[i] = contA
-        document.getElementById(`setA${i}`).innerHTML = setA[i]
-        setB[i] = contB
-        document.getElementById(`setB${i}`).innerHTML = setB[i]
-
-        contB = 0
-        document.getElementById("valorB").innerHTML = contB
-        contA = 0
-        document.getElementById("valorA").innerHTML = contA
-        ganhosB++
-        document.getElementById("setganhob").innerHTML = ganhosB
-        i++
-        sets++
-    }
-    if (sets == 5 && contB == 15) {
-        setA[i] = contA
-        document.getElementById(`setA${i}`).innerHTML = setA[i]
-        setB[i] = contB
-        document.getElementById(`setB${i}`).innerHTML = setB[i]
-
-        contB = 0
-        document.getElementById("valorB").innerHTML = contB
-        contA = 0
-        document.getElementById("valorA").innerHTML = contA
-        ganhosB++
-        document.getElementById("setganhob").innerHTML = ganhosB
-        i++
-        sets++
-    }
+    if (contB-contA > 1 ) {
+        if (contB == ganhar && sets < 5) {
+            setA[i] = contA
+            document.getElementById(`setA${i}`).innerHTML = setA[i]
+            setB[i] = contB
+            document.getElementById(`setB${i}`).innerHTML = setB[i]
+    
+            contB = 0
+            document.getElementById("valorB").innerHTML = contB
+            contA = 0
+            document.getElementById("valorA").innerHTML = contA
+            ganhosB++
+            document.getElementById("setganhob").innerHTML = ganhosB
+            i++
+            sets++
+            ganhar = 25
+        }
+        if (sets == 5 && contB == 15) {
+            setA[i] = contA
+            document.getElementById(`setA${i}`).innerHTML = setA[i]
+            setB[i] = contB
+            document.getElementById(`setB${i}`).innerHTML = setB[i]
+    
+            contB = 0
+            document.getElementById("valorB").innerHTML = contB
+            contA = 0
+            document.getElementById("valorA").innerHTML = contA
+            ganhosB++
+            document.getElementById("setganhob").innerHTML = ganhosB
+            i++
+            sets++
+        }
+    } 
     if (ganhosB == 3) {
         document.getElementById("aparv").innerHTML = `<section id="vitorioso"><strong>Equipe B venceu!✔</strong></section>`
         contB = 0
@@ -49,6 +52,9 @@ function adicionarB() {
         contA = 0
         document.getElementById("valorB").innerHTML = contB
         document.getElementById("valorA").innerHTML = contA
+    }
+    if(contA+contB == ganhar*2-2){
+        ganhar++
     }
 }
 
@@ -68,39 +74,42 @@ var setA = []
 var ganhosA = 0
 
 function adicionarA() {
-    if (contA != 25) {
+    if (contA != ganhar) {
         contA++
         document.getElementById("valorA").innerHTML = contA
     }
-    if (contA == 25 && sets < 5) {
-        setA[i] = contA
-        document.getElementById(`setA${i}`).innerHTML = setA[i]
-        setB[i] = contB
-        document.getElementById(`setB${i}`).innerHTML = setB[i]
-
-        contB = 0
-        document.getElementById("valorB").innerHTML = contB
-        contA = 0
-        document.getElementById("valorA").innerHTML = contA
-        ganhosA++
-        document.getElementById("setganhoa").innerHTML = ganhosA
-        i++
-        sets++
-    }
-    if (sets == 5 && contA == 15) {
-        setA[i] = contA
-        document.getElementById(`setA${i}`).innerHTML = setA[i]
-        setB[i] = contB
-        document.getElementById(`setB${i}`).innerHTML = setB[i]
-
-        contB = 0
-        document.getElementById("valorB").innerHTML = contB
-        contA = 0
-        document.getElementById("valorA").innerHTML = contA
-        ganhosA++
-        document.getElementById("setganhob").innerHTML = ganhosA
-        i++
-        sets++
+    if (contA-contB > 1 ) {
+        if (contA == ganhar && sets < 5) {
+            setA[i] = contA
+            document.getElementById(`setA${i}`).innerHTML = setA[i]
+            setB[i] = contB
+            document.getElementById(`setB${i}`).innerHTML = setB[i]
+    
+            contB = 0
+            document.getElementById("valorB").innerHTML = contB
+            contA = 0
+            document.getElementById("valorA").innerHTML = contA
+            ganhosA++
+            document.getElementById("setganhoa").innerHTML = ganhosA
+            i++
+            sets++
+            ganhar = 25
+        }
+        if (sets == 5 && contA == 15) {
+            setA[i] = contA
+            document.getElementById(`setA${i}`).innerHTML = setA[i]
+            setB[i] = contB
+            document.getElementById(`setB${i}`).innerHTML = setB[i]
+    
+            contB = 0
+            document.getElementById("valorB").innerHTML = contB
+            contA = 0
+            document.getElementById("valorA").innerHTML = contA
+            ganhosA++
+            document.getElementById("setganhob").innerHTML = ganhosA
+            i++
+            sets++
+        }
     }
     if (ganhosA == 3) {
         document.getElementById("aparv").innerHTML = `<section id="vitorioso"><strong>Equipe A venceu!✔</strong></section>`
@@ -115,6 +124,9 @@ function adicionarA() {
         document.getElementById("valorB").innerHTML = contB
         document.getElementById("valorA").innerHTML = contA
     }
+    if(contA+contB == (ganhar*2)-2){
+        ganhar++
+    }
 }
 
 function subtrairA() {
@@ -126,8 +138,10 @@ function subtrairA() {
 
 
 
-
+if(contA+contB == (ganhar*2)-2){
+    ganhar++
+}
 
 var sets = 1
 var i = 1
-
+var ganhar = 25
