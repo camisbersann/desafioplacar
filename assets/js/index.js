880b449cc0e6fbc656f1dd2faba42e7fcebb5d35
Liviadbelao <125027552+Liviadbelao@users.noreply.github.com>
2023-05-15 16:10:02 -0300

    fix gif

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,67 +1,75 @@
-var contB = 0
-var setB = []
-var ganhosB = 0
+var contB = 0;
+var setB = [];
+var ganhosB = 0;
 
 function adicionarB() {
     if (contB != ganhar) {
-        contB++
-        document.getElementById("valorB").innerHTML = contB
+        contB++;
+        document.getElementById("valorB").innerHTML = contB;
     }
     if (contB-contA > 1 ) {
         if (contB == ganhar && sets < 5) {
-            setA[i] = contA
-            document.getElementById(`setA${i}`).innerHTML = setA[i]
-            setB[i] = contB
-            document.getElementById(`setB${i}`).innerHTML = setB[i]
+            setA[sets] = contA;
+            document.getElementById(`setA${sets}`).innerHTML = setA[sets];
+            setB[sets] = contB;
+            document.getElementById(`setB${sets}`).innerHTML = setB[sets];
     
-            contB = 0
-            document.getElementById("valorB").innerHTML = contB
-            contA = 0
-            document.getElementById("valorA").innerHTML = contA
-            ganhosB++
-            document.getElementById("setganhob").innerHTML = ganhosB
-            i++
-            sets++
-            ganhar = 25
+            contB = 0;
+            document.getElementById("valorB").innerHTML = contB;
+            contA = 0;
+            document.getElementById("valorA").innerHTML = contA;
+            ganhosB++;
+            document.getElementById("setganhob").innerHTML = ganhosB;
+            i++;
+            sets++;
+            ganhar = 25;
         }
-        if (sets == 5 && contB == 15) {
-            setA[i] = contA
-            document.getElementById(`setA${i}`).innerHTML = setA[i]
-            setB[i] = contB
-            document.getElementById(`setB${i}`).innerHTML = setB[i]
+        if (sets == 5 && contB == ganhar) {
+            setA[sets] = contA;
+            document.getElementById(`setA${sets}`).innerHTML = setA[sets];
+            setB[sets] = contB;
+            document.getElementById(`setB${sets}`).innerHTML = setB[sets];
     
-            contB = 0
-            document.getElementById("valorB").innerHTML = contB
-            contA = 0
-            document.getElementById("valorA").innerHTML = contA
-            ganhosB++
-            document.getElementById("setganhob").innerHTML = ganhosB
-            i++
-            sets++
+            contB = 0;
+            document.getElementById("valorB").innerHTML = contB;
+            contA = 0;
+            document.getElementById("valorA").innerHTML = contA;
+            ganhosB++;
+            document.getElementById("setganhob").innerHTML = ganhosB;
+            i++;
+            sets++;
         }
     } 
     if (ganhosB == 3) {
-        document.getElementById("aparv").innerHTML = `<section id="vitorioso"><strong>Equipe B venceu!✔</strong></section>`
-        contB = 0
-        document.getElementById("valorB").innerHTML = contB
-        contA = 0
-        document.getElementById("valorA").innerHTML = contA
+        document.getElementById("aparv").innerHTML = `<section id="vitorioso"><strong>Equipe B venceu!✔</strong></section>`;
+        contB = 0;
+        document.getElementById("valorB").innerHTML = contB;
+        contA = 0;
+        document.getElementById("valorA").innerHTML = contA;
+        document.getElementById("bloco2").style.backgroundImage = 'url(https://usagif.com/wp-content/uploads/firework-1.gif)';
+        document.getElementById("bloco2").style.color = 'white';
     }
     if (ganhosA == 3) {
-        contB = 0
-        contA = 0
-        document.getElementById("valorB").innerHTML = contB
-        document.getElementById("valorA").innerHTML = contA
+        contB = 0;
+        contA = 0;
+        document.getElementById("valorB").innerHTML = contB;
+        document.getElementById("valorA").innerHTML = contA;
+        document.getElementById("bloco2").style.backgroundImage = 'url(https://usagif.com/wp-content/uploads/firework-1.gif)';
+        document.getElementById("bloco2").style.color = 'white';
     }
     if(contA+contB == ganhar*2-2){
-        ganhar++
+        ganhar++;
+    }
+    if(sets == 5 && i == 5) {
+        ganhar = 15;
+        i++;
     }
 }
 
 function subtrairB() {
     if (contB > 0) {
-        contB--
-        document.getElementById("valorB").innerHTML = contB
+        contB--;
+        document.getElementById("valorB").innerHTML = contB;
     }
 }
 
@@ -69,79 +77,90 @@ function subtrairB() {
 
 
 
-var contA = 0
-var setA = []
-var ganhosA = 0
+var contA = 0;
+var setA = [];
+var ganhosA = 0;
 
 function adicionarA() {
     if (contA != ganhar) {
-        contA++
-        document.getElementById("valorA").innerHTML = contA
+        contA++;
+        document.getElementById("valorA").innerHTML = contA;
     }
     if (contA-contB > 1 ) {
         if (contA == ganhar && sets < 5) {
-            setA[i] = contA
-            document.getElementById(`setA${i}`).innerHTML = setA[i]
-            setB[i] = contB
-            document.getElementById(`setB${i}`).innerHTML = setB[i]
+            setA[sets] = contA;
+            document.getElementById(`setA${sets}`).innerHTML = setA[sets];
+            setB[sets] = contB;
+            document.getElementById(`setB${sets}`).innerHTML = setB[sets];
     
-            contB = 0
-            document.getElementById("valorB").innerHTML = contB
-            contA = 0
-            document.getElementById("valorA").innerHTML = contA
-            ganhosA++
-            document.getElementById("setganhoa").innerHTML = ganhosA
-            i++
-            sets++
-            ganhar = 25
+            contB = 0;
+            document.getElementById("valorB").innerHTML = contB;
+            contA = 0;
+            document.getElementById("valorA").innerHTML = contA;
+            ganhosA++;
+            document.getElementById("setganhoa").innerHTML = ganhosA;
+            i++;
+            sets++;
+            ganhar = 25;
         }
-        if (sets == 5 && contA == 15) {
-            setA[i] = contA
-            document.getElementById(`setA${i}`).innerHTML = setA[i]
-            setB[i] = contB
-            document.getElementById(`setB${i}`).innerHTML = setB[i]
+        if (sets == 5 && contA == ganhar) {
+            setA[sets] = contA;
+            document.getElementById(`setA${sets}`).innerHTML = setA[sets];
+            setB[sets] = contB;
+            document.getElementById(`setB${sets}`).innerHTML = setB[sets];
     
-            contB = 0
-            document.getElementById("valorB").innerHTML = contB
-            contA = 0
-            document.getElementById("valorA").innerHTML = contA
-            ganhosA++
-            document.getElementById("setganhob").innerHTML = ganhosA
-            i++
-            sets++
+            contB = 0;
+            document.getElementById("valorB").innerHTML = contB;
+            contA = 0;
+            document.getElementById("valorA").innerHTML = contA;
+            ganhosA++;
+            document.getElementById("setganhob").innerHTML = ganhosA;
+            i++;
+            sets++;
         }
     }
     if (ganhosA == 3) {
-        document.getElementById("aparv").innerHTML = `<section id="vitorioso"><strong>Equipe A venceu!✔</strong></section>`
-        contB = 0
-        contA = 0
-        document.getElementById("valorB").innerHTML = contB
-        document.getElementById("valorA").innerHTML = contA
+        document.getElementById("aparv").innerHTML = `<section id="vitorioso"><strong>Equipe A venceu!✔</strong></section>`;
+        contB = 0;
+        contA = 0;
+        document.getElementById("valorB").innerHTML = contB;
+        document.getElementById("valorA").innerHTML = contA;
+        document.getElementById("bloco2").style.backgroundImage = 'url(https://usagif.com/wp-content/uploads/firework-1.gif)';
+        document.getElementById("bloco2").style.color = 'white';
     }
     if (ganhosB == 3) {
-        contB = 0
-        contA = 0
-        document.getElementById("valorB").innerHTML = contB
-        document.getElementById("valorA").innerHTML = contA
+        contB = 0;
+        contA = 0;
+        document.getElementById("valorB").innerHTML = contB;
+        document.getElementById("valorA").innerHTML = contA;
+        document.getElementById("bloco2").style.backgroundImage = 'url(https://usagif.com/wp-content/uploads/firework-1.gif)';
+        document.getElementById("bloco2").style.color = 'white';
     }
     if(contA+contB == (ganhar*2)-2){
-        ganhar++
+        ganhar++;
+    }
+    if(sets == 5 && i == 5) {
+        ganhar = 15;
+        i++;
     }
 }
 
 function subtrairA() {
     if (contA > 0) {
-        contA--
-        document.getElementById("valorA").innerHTML = contA
+        contA--;
+        document.getElementById("valorA").innerHTML = contA;
     }
 }
 
 
 
 if(contA+contB == (ganhar*2)-2){
-    ganhar++
+    ganhar++;
 }
+var sets = 1;
+var i = 1;
+var ganhar = 25;
 
-var sets = 1
-var i = 1
-var ganhar = 25
+if(sets == 5 && i == 5) {
+    ganhar = 15;
+}
